9f45556da7af28162dedd2351e618a45d9adf741
Vitalii Doroshenko <2328221@ukr.net>
2023-01-12 15:30:43 +0200

    mongo22

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://test:test22@cluster0.rcivy.azure.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`Servak bezhit na ${PORT}`)))
+.catch(err => console.log(err.message))
+
