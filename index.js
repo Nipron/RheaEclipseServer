f572c19dcd77b8f893cd65785228814bdbca57c1
Vitalii Doroshenko <2328221@ukr.net>
2023-01-14 22:00:02 +0200

    hhh

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = "mongodb+srv://test:test22@cluster0.rcivy.azure.mongodb.net/?retryWrites=true&w=majority"
+//const CONNECTION_URL = "mongodb+srv://test:test22@cluster0.rcivy.azure.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://boba:boba@cluster0.rcivy.azure.mongodb.net/?retryWrites=true&w=majority"
+
 const PORT = process.env.PORT || 5000
 
 mongoose.set('strictQuery', false)
-
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Servak bezhit na ${PORT}`)))
 .catch(err => console.log(err.message))
